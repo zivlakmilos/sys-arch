b0ce6a4581bd5557841d3985ad61f1e87d687df1
Milos Zivlak <zivlakmilos@gmail.com>
2023-06-02 07:46:54 +0200

    Test Docker image with XOrg support

--- start-dockers.sh
+++ start-dockers.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-docker run --rm -it new-computer bash
+Xephyr :1 -ac -br -screen 1024x768 -resizeable -reset -terminate &
+docker run -e DISPLAY=:1 -v /tmp/.X11-unix:/tmp/.X11-unix --rm -it --name new-computer --mount type=bind,source="$(pwd)",target=/home/zi/sys-arch new-computer bash
+
+#docker run --rm -it --name new-computer --mount type=bind,source="$(pwd)",target=/home/zi/sys-arch new-computer bash
