f0359b35ac9e42dd53e516c33faf3dff3aec2de0
Milos Zivlak <zivlakmilos@gmail.com>
2023-08-16 15:29:33 +0200

    SSH and keys

--- ansible-run.sh
+++ ansible-run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ansible-playbook local.yml --ask-become-pass
+ansible-playbook local.yml --ask-become-pass --ask-vauld-pass
 
 #sudo pacman -S ansible
 #ansible-pull -U https://github.com/zivlakmilos/sys-arch.git
